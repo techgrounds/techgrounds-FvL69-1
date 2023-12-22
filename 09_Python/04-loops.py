235d533e48cb137abf62b51f48a7be77f81ac608
fvl <fvlieshout@outlook.com>
2023-12-22 16:55:45 +0100

    python loops

---
+++ 09_Python/04-loops.py
@@ -0,0 +1,20 @@
+print("Exercise 1")
+# Use a while loop to print the value of x in every iteration of the loop.
+x = 1
+while x <= 10:
+    print(x)
+    x += 1
+
+print()
+print("Exercise 2")
+# Using the for loop, print the value of x multiplied by the value of i, for up to 50 iterations.
+x = 5
+for i in range(51):
+    print(i*x)
+
+print()
+print("Exercise 3")
+# Use a for loop to loop over the array. Print every name individually.
+arr = ["Shikha", "Casper", "Bart", "Ruben", "Ulviye"]
+for name in arr:
+    print(name)
