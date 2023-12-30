71c9f4568ebb7a37860f6afab94f76c504531de2
fvl <fvlieshout@outlook.com>
2023-12-30 20:07:28 +0100

    python update

--- 09_Python/08-key-value_pairs.py
+++ 09_Python/08-key-value_pairs.py
@@ -18,6 +18,33 @@ for x in dict.keys():
 
 print(dict)
 
+
 print()
 import csv 
 # Write the dict content to a csv file. 
+# Create a csv file.
+fp = open("dict.csv", "w")
+fp.close() 
+
+# Using the key list 'k' for the header.
+csv_header = k 
+# Store a copy of the dict in an empty list.
+dict_list = []
+dict_list.append(dict.copy())
+
+file_csv = "dict.csv"
+try:
+    with open(file_csv, 'a') as csv_file:
+        linker = csv.DictWriter(csv_file, fieldnames = csv_header)
+        linker.writeheader()
+        for item in dict_list:
+            linker.writerow(item)
+
+except IOError:
+    print("Error encountered...")
+
+with open("dict.csv") as f:
+    buffer = f.read()
+
+print(buffer)
+
